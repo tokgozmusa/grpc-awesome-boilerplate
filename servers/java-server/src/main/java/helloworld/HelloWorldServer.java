48e847458324cc6b086d81788af94215b65b7f34
tokgozmusa <tokgozmusa@gmail.com>
2018-08-27 01:26:40 +0200

    Reformat code

--- servers/java-server/src/main/java/helloworld/HelloWorldServer.java
+++ servers/java-server/src/main/java/helloworld/HelloWorldServer.java
@@ -8,51 +8,49 @@ import java.util.Arrays;
 import java.util.logging.Logger;
 
 /**
- * User: tokgozmusa
- * Date: 25.08.2018
- * Time: 00:23
+ * User: tokgozmusa Date: 25.08.2018 Time: 00:23
  **/
 public class HelloWorldServer {
 
-    public static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+  public static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
 
-    private int port = 42420;
-    private Server server;
+  private int port = 42420;
+  private Server server;
 
-    public static void main(String[] args) throws Exception {
-        logger.info("Server startup. Args = " + Arrays.toString(args));
-        final HelloWorldServer helloWorldServer = new HelloWorldServer();
-        helloWorldServer.start();
-        helloWorldServer.blockUntilShutdown();
+  public static void main(String[] args) throws Exception {
+    logger.info("Server startup. Args = " + Arrays.toString(args));
+    final HelloWorldServer helloWorldServer = new HelloWorldServer();
+    helloWorldServer.start();
+    helloWorldServer.blockUntilShutdown();
+  }
+
+  private void start() throws Exception {
+    logger.info("Starting the grpc server");
+
+    server = ServerBuilder.forPort(port)
+        .addService(new HelloWorldService())
+        .build()
+        .start();
+
+    logger.info("Server started. Listening on port " + port);
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      System.err.println("*** JVM is shutting down. Turning off grpc server as well ***");
+      HelloWorldServer.this.stop();
+      System.err.println("*** shutdown complete ***");
+    }));
+  }
+
+  private void stop() {
+    if (server != null) {
+      server.shutdown();
     }
+  }
 
-    private void start() throws Exception {
-        logger.info("Starting the grpc server");
-
-        server = ServerBuilder.forPort(port)
-                .addService(new HelloWorldService())
-                .build()
-                .start();
-
-        logger.info("Server started. Listening on port " + port);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.err.println("*** JVM is shutting down. Turning off grpc server as well ***");
-            HelloWorldServer.this.stop();
-            System.err.println("*** shutdown complete ***");
-        }));
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
     }
-
-    private void stop() {
-        if (server != null) {
-            server.shutdown();
-        }
-    }
-
-    private void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-            server.awaitTermination();
-        }
-    }
+  }
 }
 
--- servers/java-server/src/main/java/helloworld/services/HelloWorldService.java
+++ servers/java-server/src/main/java/helloworld/services/HelloWorldService.java
@@ -9,22 +9,21 @@ import io.grpc.stub.StreamObserver;
 import java.time.LocalDateTime;
 
 /**
- * User: tokgozmusa
- * Date: 26.08.2018
- * Time: 01:42
+ * User: tokgozmusa Date: 26.08.2018 Time: 01:42
  **/
 public class HelloWorldService extends HelloWorldGrpc.HelloWorldImplBase {
 
-    @Override
-    public void sayHello(HelloRequest helloRequest, StreamObserver<HelloResponse> responseObserver) {
-        HelloWorldServer.logger.info("Request received:" + LocalDateTime.now());
+  @Override
+  public void sayHello(HelloRequest helloRequest, StreamObserver<HelloResponse> responseObserver) {
+    HelloWorldServer.logger.info("Request received:" + LocalDateTime.now());
 
-        HelloResponse helloResponse = HelloResponse.newBuilder().setMessage(sayHelloImpl(helloRequest.getName())).build();
-        responseObserver.onNext(helloResponse);
-        responseObserver.onCompleted();
-    }
+    HelloResponse helloResponse = HelloResponse.newBuilder()
+        .setMessage(sayHelloImpl(helloRequest.getName())).build();
+    responseObserver.onNext(helloResponse);
+    responseObserver.onCompleted();
+  }
 
-    private String sayHelloImpl(String name) {
-        return "Hello " + name;
-    }
+  private String sayHelloImpl(String name) {
+    return "Hello " + name;
+  }
 }
